6929bbe793b992888ddd42a63e6591124d05f1e4
MengChi <mc2311362372@gmail.com>
2017-08-16 19:19:41 +0800

    ··

---
+++ UI/Assets/Script/AssetBundleTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleTest : MonoBehaviour {
+
+    IEnumerator Start()
+    {
+        WWW www = new WWW("http://myserver/myBundle.unity3d");
+        yield return www;
+
+        // Get the designated main asset and instantiate it.
+        Instantiate(www.assetBundle.mainAsset);
+    }
+}
