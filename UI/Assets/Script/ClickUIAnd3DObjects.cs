c8dde09f05eb3152b3a31effeee8505e71d25cc0
MengChi <mc2311362372@gmail.com>
2017-08-18 17:27:18 +0800

    ··

---
+++ UI/Assets/Script/ClickUIAnd3DObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickUIAnd3DObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PointerDown() {
+        Debug.Log("按下");
+    }
+}
