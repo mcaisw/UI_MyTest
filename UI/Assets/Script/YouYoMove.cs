9424b83ee9aeef4f7694b36ffcee917fd4d14477
MengChi <mc2311362372@gmail.com>
2017-09-20 16:01:29 +0800

    ··

---
+++ UI/Assets/Script/YouYoMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class YouYoMove : MonoBehaviour {
+
+    Tween t;
+    Tween t_b;
+    // Use this for initialization
+    void Start()
+    {
+        t = DOTween.To(() => this.transform.position,
+                        x => this.transform.position = x,
+                        new Vector3(this.transform.position.x + 1, this.transform.position.y, this.transform.position.z),
+                        1).SetDelay(1)
+                        .OnComplete(() =>
+                        {
+                            t_b = DOTween.To(() => this.transform.position,
+                               x => this.transform.position = x,
+                               new Vector3(this.transform.position.x - 1, this.transform.position.y, this.transform.position.z),
+                               1).SetDelay(1).OnComplete(PlayDotween);
+                        }
+                         );
+    }
+
+    void PlayDotween()
+    {
+        t = DOTween.To(() => this.transform.position,
+                        x => this.transform.position = x,
+                        new Vector3(this.transform.position.x + 1, this.transform.position.y, this.transform.position.z),
+                        1).SetDelay(1)
+                        .OnComplete(() =>
+                        {
+                            t_b = DOTween.To(() => this.transform.position,
+                               x => this.transform.position = x,
+                               new Vector3(this.transform.position.x - 1, this.transform.position.y, this.transform.position.z),
+                               1).SetDelay(1).OnComplete(PlayDotween);
+                        }
+                         );
+    }
+}
